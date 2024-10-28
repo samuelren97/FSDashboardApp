b9c95d15adb3ebc64883af69e42e9ce8b2aabdab
Samuel Renaud <2392263@csfoy.ca>
2024-10-27 20:09:29 -0400

    Added API support for new endpoints

--- app/src/main/java/com/shunta/fsdashboard/networking/APIService.kt
+++ app/src/main/java/com/shunta/fsdashboard/networking/APIService.kt
@@ -3,6 +3,7 @@ package com.shunta.fsdashboard.networking
 import okhttp3.ResponseBody
 import retrofit2.Call
 import retrofit2.http.Body
+import retrofit2.http.POST
 import retrofit2.http.PUT
 import retrofit2.http.Path
 
@@ -17,4 +18,22 @@ interface APIService {
         @Path("id") id: Int,
         @Body freq: String,
     ): Call<ResponseBody>
+
+    @PUT("ap/hdg")
+    fun putHdg(@Body hdg: Float): Call<ResponseBody>
+
+    @PUT("ap/alt")
+    fun putAlt(@Body alt: Int): Call<ResponseBody>
+
+    @PUT("ap/master")
+    fun putMaster(): Call<ResponseBody>
+
+    @POST("ap/hdg")
+    fun postHdg(): Call<ResponseBody>
+
+    @PUT("ap/nav")
+    fun putNav(): Call<ResponseBody>
+
+    @PUT("ap/flc")
+    fun putFlc: Call<ResponseBody>
 }
